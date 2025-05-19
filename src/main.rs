3ae15824036367862f48278a1b182a334d9fddc0
rorikstr <slava.070299@gmail.com>
2025-05-19 21:01:27 +0300

    fixed my hands

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ fn main() {
 
     let config = Config::new(&args);
 
+    let config = match config {
+        Ok(val) => val,
+        Err(msg) => {
+            println!("{}", msg);
+            return;
+        },
+    };
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
@@ -23,10 +31,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough args");
+        }
         let query = args[0].clone();
         let file_path = args[1].clone();
         
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
