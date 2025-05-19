3e32be2fb28aa97d1988c0644f335fe30af4ebee
rorikstr <slava.070299@gmail.com>
2025-05-19 20:20:28 +0300

    fixed

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,8 @@ struct Config {
 }
 
 fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+    let query = args[0].clone();
+    let file_path = args[1].clone();
 
     Config { query, file_path }
 }
